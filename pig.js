e1c97666083a928a9956cd786b8603bffc9acdd8
tattavgupta <72901330+tattavgupta@users.noreply.github.com>
2020-11-03 08:34:16 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,21 @@
+class Pig{
+    constructor(x,y){
+        var pig_options={restitution:0.8,friction:0.3,density:1.0}
+        this.body=Bodies.rectangle(x,y,50,50)
+        this.width=50
+        this.height=50
+        World.add(world,this.body)
+    }
+
+    display(){
+        var pos=this.body.position
+        var angle=this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        fill("green")
+        rectMode(CENTER)
+        rect(0,0,this.width,this.height)
+        pop()
+    }
+}
